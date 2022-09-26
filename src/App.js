fbda446d59b48292758182b4e3b40b2e1fec54e8
Ashik045 <ashikurislam045@gmail.com>
2022-09-26 20:50:23 +0600

    ready to work

--- src/App.js
+++ src/App.js
@@ -1,7 +1,9 @@
+import Navbar from './Components/Navbar/Navbar';
+
 function App() {
     return (
         <div className="App">
-            <h1>React Admin Dashboard</h1>
+            <Navbar />
         </div>
     );
 }
---
+++ src/Components/Navbar/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Navbar() {
+    return (
+        <div>
+            <h1>Navbar - Admin Dashboard</h1>
+        </div>
+    );
+}
+
+export default Navbar;
